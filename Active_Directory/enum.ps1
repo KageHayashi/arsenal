8357e8f7a6b894c390211c28288839d576d0156b
KageHayashi <38701476+KageHayashi@users.noreply.github.com>
2021-09-09 09:38:49 -0700

    Update enum.ps1

--- Active_Directory/enum.ps1
+++ Active_Directory/enum.ps1
@@ -18,6 +18,7 @@ $objDomain = New-Object System.DirectoryServices.DirectoryEntry
 $Searcher.SearchRoot = $objDomain
 
 <# Filter search. Refer to samAccountType filter #>
+<# Current filter enumerates for all users in the AD #>
 $Searcher.filter="samAccountType=805306368"
 
 $Searcher.FindAll()
