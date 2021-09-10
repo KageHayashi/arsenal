53916508ca566c455473bd11f4ff34ed9e2de136
KageHayashi <kagehayashi@gmail.com>
2021-09-10 08:46:42 -0700

    Added misc folder and created wlan_password_grab

---
+++ misc/wlan_password_grab.ps1
@@ -0,0 +1,22 @@
+$all_profiles = netsh wlan show profiles | Select-String '\:(.+)$'
+$all_clear = @()
+$no_clear = @()
+
+foreach ($profile in $all_profiles) {
+    $name = $profile.Matches.Groups[1].Value.Trim();
+    $pass_match = netsh wlan show profile name=$name key=clear | Select-String 'Key Content\W+\:(.+)$';
+    try {
+        $pass = $pass_match.Matches.Groups[1].Value.Trim();
+        $all_clear += ($name + ':' + $pass);
+    }
+    catch {
+        $no_clear += ($name)
+    }
+}
+Write-Host 'All Clear Keys Saved'
+Write-Host '--------------------'
+$all_clear
+Write-Host 
+Write-Host 'No Clear Keys Saved'
+Write-Host '-------------------'
+$no_clear
